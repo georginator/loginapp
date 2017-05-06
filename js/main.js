bf5e440c84471d4d612e7039f28064e290c6cc12
George Franco <georgefranco7@gmail.com>
2017-05-05 21:38:29 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,34 +1,71 @@
 
 //array/list of users
-var objPeople = [
+var zodiac = [
 	{
-		username: "george",
-		password: "franco"
+		sign: "aquarius",
+		info:"Progressive, original, independent, humanitarian"
 	},
 	{
-		username:"isaac",
-		password:"hartwick"
+		sign:"pisces",
+		info:" Compassionate, artistic, intuitive, gentle, wise, musical"
 	},
 	{
-		username:"matt",
-		password:"childs"
+		sign:"aries",	
+		info:"Courageous, determined, confident, enthusiastic, optimistic, honest, passionate"
+	},
+	{
+		sign:"taurus",
+		info:"Reliable, patient, practical, devoted, responsible, stable"
+	},
+	{
+		sign:"gemini",
+		info:"Gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas"
+	},
+	{
+		sign:"cancer",
+		info:"Tenacious, highly imaginative, loyal, emotional, sympathetic, persuasive"
+	},
+	{
+		sign:"leo",
+		info:"Creative, passionate, generous, warm-hearted, cheerful, humorous"
+	},
+	{
+		sign:"virgo",
+		info:"Loyal, analytical, kind, hardworking, practical"
+	},
+	{
+		sign:"libra",
+		info:"Cooperative,diplomatic, gracious, fair-minded, social"
+	},
+	{
+		sign:"scorpio",
+		info:"Resourceful, brave, passionate, stubborn, a true friend"
+	},
+	{
+		sign:"sagittarius",
+		info:"Generous, idealistic, great sense of humor"
+	},
+	{
+		sign:"capricorn",
+		info:"Responsible, disciplined, self-control, good managers"
 	}
 ]
 
 
 
-function getInfo (){
-	var username = document.getElementById("username").value
-	var password = document.getElementById("password").value
+function getInfo () {
+
+	var userdata = document.getElementById("userdata").value
+	
 
 
-	for(i = 0; i < objPeople.length; i++) {
-		if(username == objPeople[i].username && password == objPeople[i].password) {
-			document.write("Welcome " + username + " ! You are logged in!")
+	for(i = 0; i < zodiac.length; i++) {
+		if(userdata == zodiac[i].sign) {
+			document.write("You are " + zodiac[i].info)
 			return
 		}
 	}
-	document.write("incorrect username or password")
+	document.write("incorrect zodiac entry")
 }
 
 
